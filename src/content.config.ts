788ad8fddf0ab11560d7d71626606ed47f20287b
Iwan <composed.shapes@gmail.com>
2025-11-07 17:29:48 +0800

    fix loading image from json

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,37 +2,73 @@ import { z, defineCollection } from "astro:content";
 import { file } from "astro/loaders";
 
 const plates = defineCollection({
-	loader: file("src/data/plates.json"),
-	schema: z.object({
-		slug: z.string(),
-		prettyName: z.string(),
-		description: z.string(),
-		plates: z.array(
-			z.object({
-				name: z.string(),
-				description: z.string(),
-				price: z.number(),
-			})
-		),
-	}),
+  loader: file("src/data/plates.json"),
+  schema: z.object({
+    slug: z.string(),
+    prettyName: z.string(),
+    description: z.string(),
+    plates: z.array(
+      z.object({
+        name: z.string(),
+        description: z.string(),
+        price: z.number(),
+      }),
+    ),
+  }),
 });
 
 const reviews = defineCollection({
-	loader: file("src/data/reviews.json"),
-	schema: z.object({
-		author: z.string(),
-		review: z.string(),
-	}),
+  loader: file("src/data/reviews.json"),
+  schema: z.object({
+    author: z.string(),
+    review: z.string(),
+  }),
 });
 
 const gallery = defineCollection({
-	loader: file("src/data/gallery.json"),
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			alt: z.string(),
-			image: image(),
-		}),
+  loader: file("src/data/gallery.json"),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      alt: z.string(),
+      image: image(),
+    }),
 });
 
-export const collections = { plates, reviews, gallery };
+const outlet = defineCollection({
+  loader: file("src/data/outlet.json"),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      openAt: z.string(),
+      whatsapp: z.string(),
+      image: image(),
+      maps: z.string(),
+    }),
+});
+
+const hotel = defineCollection({
+  loader: file("src/data/hotel.json"),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      openAt: z.string(),
+      whatsapp: z.string(),
+      image: image(),
+      maps: z.string(),
+    }),
+});
+
+const dentes = defineCollection({
+  loader: file("src/data/dentes.json"),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      openAt: z.string(),
+      whatsapp: z.string(),
+      image: image(),
+      maps: z.string(),
+    }),
+});
+
+export const collections = { plates, reviews, gallery, outlet, hotel, dentes };
