79cdf1244d4773df00f6970c7bca9c0c5212325e
Iwan <composed.shapes@gmail.com>
2025-11-07 16:18:08 +0800

    added favicon.ico and outlet in about-us page

--- src/data/menus.js
+++ src/data/menus.js
@@ -1,61 +1,57 @@
 export const menuMain = [
-    {
-        name: "Halaman Depan",
-        url: "/",
-        highlighted: false,
-    },
-    {
-        name: "Varian Bakpia",
-        url: "/menu",
-        highlighted: false,
-    },
-    {
-        name: "Kisah Kami",
-        url: "/about-us",
-        highlighted: false,
-    },
-    // {
-    //     name: "Gallery",
-    //     url: "/gallery",
-    //     highlighted: false,
-    // },
-    // {
-    //     name: "Contact",
-    //     url: "/contact",
-    //     highlighted: false,
-    // },
-    {
-        name: "Pesan Bakpia",
-        url: "https://wa.me/6282138060002?text=Halo,%20saya%20ingin%20membeli%20bakpia%203%20generasi%20", 
-        highlighted: true,
-        target:"_blank",
-    },
+  {
+    name: "Halaman Depan",
+    url: "/",
+    highlighted: false,
+  },
+  {
+    name: "Varian Bakpia",
+    url: "/menu",
+    highlighted: false,
+  },
+  {
+    name: "Tentang Kami",
+    url: "/about-us",
+    highlighted: false,
+  },
+  // {
+  //     name: "Gallery",
+  //     url: "/gallery",
+  //     highlighted: false,
+  // },
+  // {
+  //     name: "Contact",
+  //     url: "/contact",
+  //     highlighted: false,
+  // },
+  {
+    name: "Pesan Bakpia",
+    url: "https://wa.me/6282138060002?text=Halo,%20saya%20ingin%20membeli%20bakpia%203%20generasi%20",
+    highlighted: true,
+    target: "_blank",
+  },
 ];
 
 export const footerPrimary = [
-    {
-        title: "Navigatie",
-        items: [
-            { name: "home", url: "/" },
-        ],
-    },
+  {
+    title: "Navigatie",
+    items: [{ name: "home", url: "/" }],
+  },
 ];
 
 export const footerSecondary = [
-    {
-        title: "support",
-        items: [
-            { name: "pricing", url: "/pricing" },
-        ],
-    },
+  {
+    title: "support",
+    items: [{ name: "pricing", url: "/pricing" }],
+  },
 ];
 
 export const footerTertiary = [
-    {
-        title: "Links",
-        items: [
-            { name: "privacybeleid", url: '/privacy-policy' },
-            { name: "api status", url: "/api-status" },
-        ],
-    },
+  {
+    title: "Links",
+    items: [
+      { name: "privacybeleid", url: "/privacy-policy" },
+      { name: "api status", url: "/api-status" },
+    ],
+  },
 ];
---
+++ src/store.ts
@@ -0,0 +1,3 @@
+import { atom } from "nanostores";
+
+export const isOpen = atom(false); // An example store for a dialog's open state
